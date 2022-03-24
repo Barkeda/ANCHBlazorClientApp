dc9228fe3a428e9879b3e56140b8f356014aabac
LAPTOP-T1J5NJSM\Ejer <filipjessen@gmail.com>
2022-03-24 09:53:51 +0100

    Add project files.

---
+++ ANCHBlazorClientApp/Server/Controllers/FileHandlerController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ANCHBlazorClientApp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileHandlerController : ControllerBase
+    {
+        // GET: api/<FileHandlerController>
+        [HttpGet]
+        public string Get()
+        {
+            return System.IO.File.ReadAllText(@"C:\test\Test.txt");
+        }
+
+    }
+}
